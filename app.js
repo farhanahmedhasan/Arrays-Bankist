bb875f772f2220e4f9b17fcfa54045e41567ba04
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-04-05 22:17:03 +0600

    Implemented Dates Funtionality

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.9, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2021-04-03T17:01:17.194Z',
+    '2021-04-04T23:36:17.929Z',
+    '2021-04-05T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -12,6 +24,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 2.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -61,14 +85,16 @@ const btnClose = document.querySelector('.form__btn--close')
 const btnSort = document.querySelector('.btn--sort')
 
 //Getting The Current Date
+
 function getDate() {
-	const today = new Date()
-	const day = String(today.getDate()).padStart(2, '0');
-	const month = String(today.getMonth() + 1).padStart(2, '0');
-	const year = today.getFullYear()
-	return [day,month,year]
+  const now = new Date()
+  const day = String(now.getDate()).padStart(2, 0)
+  const month = String(now.getMonth() + 1).padStart(2,0)
+  const year = now.getFullYear() //.slice('2')
+  const hour = now.getHours()
+  const min = `${now.getMinutes()}`.padStart(2,0)
+  labelDate.textContent = `${day}/${month}/${year} , Time: ${hour}:${min}`
 }
-labelDate.textContent = `${getDate()[0]}/${getDate()[1]}/${getDate()[2]}`
 
 // Set The Timer
 function startTimer(duration, display) {
@@ -106,7 +132,8 @@ let currentAccount;
 
 //--------------------------------------Update the UI-------------------------------------
 function updateUI(acc) {
-  displayMovements(acc.movements)
+  getDate()
+  displayMovements(acc)
   displayCurrentBalance(acc)
   displayTotalDeposite(acc.movements)
   displayTotalWithdrew(acc.movements)
@@ -157,14 +184,39 @@ btnClose.addEventListener('click', (e) => {
 
 
 //-----------------------------------------Movements Functionality-------------------------------
+//Formate Movement Dates 
+function formateMovementDates(dates) {
+  const calcDaysPassed = (day1, day2) => Math.round((Math.abs(day2 - day1) / (1000 * 60 * 60 * 24)))
+  const daysPassed = calcDaysPassed(new Date(), dates);
+
+  if (daysPassed === 0) return `Today`
+  if (daysPassed === 1) return `Yesterday`
+  if (daysPassed <= 7) return `${daysPassed} days ago`
+  else {
+    const day = String(dates.getDate()).padStart(2, 0)
+    const month = String(dates.getMonth() + 1).padStart(2, 0)
+    const year = dates.getFullYear()
+    return `${day}/${month}/${year}`  
+  }
+}
+
 //Display The Movements
-function displayMovements(movements) {
+
+function displayMovements(acc, sort= false) {
   containerMovements.innerHTML = ''
-  movements.forEach((mov, i) => {
+  //Sorting Movements Array
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements
+  
+  movs.forEach((mov, i) => {
     const type = mov < 0 ? `withdrawal` : `deposit`
+
+    const date = new Date(acc.movementsDates[i])
+    const displayDate = formateMovementDates(date)
+
     const html = `<div class="movements__row">
-				<div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-				<div class="movements__value">${Math.abs(mov)}৳</div>
+				<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__date">${displayDate}</div>
+				<div class="movements__value">${Math.abs(mov).toFixed(2)}৳</div>
 			</div>`
     containerMovements.insertAdjacentHTML('afterbegin', html)
   })
@@ -179,7 +231,7 @@ const sum = (accumulator, curMov) => accumulator + curMov
 //Calculate the Balance: With reduceMethod
 function displayCurrentBalance(accs) {
   accs.balance = accs.movements.reduce(sum,0)//CallBack Function
-  labelBalance.textContent = accs.balance + '৳'
+  labelBalance.textContent = accs.balance.toFixed(2) + '৳'
 }
 
 //Calulate Total Deposites
@@ -187,7 +239,7 @@ function displayTotalDeposite(accs) {
   const incomes = accs
     .filter(mov => mov > 0)
     .reduce(sum) //CallBack Function
-  labelSumIn.textContent = `${incomes}৳`
+  labelSumIn.textContent = `${incomes.toFixed(2)}৳`
 }
 
 //Calulate Total Withdrawal
@@ -196,7 +248,7 @@ function displayTotalWithdrew(accs) {
   if (!expenses.length) labelSumOut.textContent = `0000৳`
   else {
     const totalExpenses = expenses.reduce(sum)
-    labelSumOut.textContent = `${Math.abs(totalExpenses)}৳`
+    labelSumOut.textContent = `${Math.abs(totalExpenses).toFixed(2)}৳`
   }
 }
 
@@ -204,8 +256,16 @@ function displayTotalWithdrew(accs) {
 function displayInterest(acc,interestRate) {
   const totalBalance = acc.balance
   const interest = (totalBalance * interestRate) / 100
-  labelSumInterest.textContent = `${interest}৳`    
+  labelSumInterest.textContent = `${interest.toFixed(2)}৳`    
 }
+
+//SORTING Happens On Button Click
+let sorted = true;
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault()
+  displayMovements(currentAccount, sorted)
+  sorted = !sorted;
+})
 
 //-----------------------------Transfer Money Functionality-----------------------------
 function transferMoney() {
@@ -219,6 +279,10 @@ function transferMoney() {
     receiverAcc && receiverAcc.userName !== currentAccount.userName) {
     currentAccount.movements.push(-amount)
     receiverAcc.movements.push(amount)
+
+    //Pushing Dates
+    currentAccount.movementsDates.push(new Date().toISOString())
+    receiverAcc.movementsDates.push(new Date().toISOString()) 
     
     //Update The UI
     updateUI(currentAccount)  
@@ -236,14 +300,13 @@ btnTransfer.addEventListener('click', (e) => {
 
 //------------------------------------Request Loan Functionality-----------------------------------
 function requestLoan() {
-  let loan = Number(inputLoanAmmount.value)
-  // const bigDeposite = currentAccount.movements.reduce((total,curValue)=> Math.max(total,curValue))
-  // const bigDeposite = Math.max(...currentAccount.movements)
+  const loan = Math.floor(inputLoanAmmount.value) //It does the type coersioin
   const anyDeposite = currentAccount.movements.some(mov => mov >= loan * .1)
 
   if (loan > 0 && anyDeposite) {
-  currentAccount.movements.push(loan)
-  updateUI(currentAccount)
+    currentAccount.movements.push(loan)
+    currentAccount.movementsDates.push(new Date().toISOString()) //Pushinf Dates
+    updateUI(currentAccount)
   }
   inputLoanAmmount.value = ''
 }
